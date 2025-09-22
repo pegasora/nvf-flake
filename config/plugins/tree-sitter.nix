180b1b2fe3c8eb7d37a1e8fe89a9380f12e66bb7
pegasora <dawsonhburgess@gmail.com>
2025-09-21 21:06:15 -0700

    essential.
    would like to one day get the "allGrammars" working, but had issues with
    nvf vs nixos for packages

--- config/plugins/tree-sitter.nix
+++ config/plugins/tree-sitter.nix
@@ -1,2 +1,32 @@
-{
+{ pkgs, ... }: {
+  vim.treesitter = {
+    enable = true;
+    grammars = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+      bash
+      c
+      comment
+      css
+      dockerfile
+      go
+      html
+      javascript
+      json
+      jsonc
+      lua
+      make
+      markdown
+      nix
+      python
+      regex
+      rust
+      toml
+      typescript
+      yaml
+      zig
+    ];
+
+    highlight.enable = true;
+    fold = true;
+    #indent.enable = true;
+  };
 }
