859fb70c2ddf1b19f4cc271ba3f4a3b6efc4f093
pegasora <dawsonhburgess@gmail.com>
2025-09-21 21:04:59 -0700

    added new paths

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -5,9 +5,14 @@
     ./lsp
 
     ./which-key.nix
-    ./nvim-tree.nix
+    #./nvim-tree.nix
+    ./oil.nix
     ./noice.nix
     ./lualine.nix
     ./supermaven.nix
+    ./telescope.nix
+    ./tree-sitter.nix
+    ./todo.nix
+    ./bufferline.nix
   ];
 }
