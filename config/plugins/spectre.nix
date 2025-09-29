e9975755ab2a15b71c57d8abbb8569641ae3ecfb
pegasora <dawsonhburgess@gmail.com>
2025-09-29 00:50:34 -0700

    added spectre

--- config/plugins/spectre.nix
+++ config/plugins/spectre.nix
@@ -1,22 +1,26 @@
 {pkgs, ...}: {
-  config.vim.lazy.plugins = {
-    "nvim-spectre" = {
-      package = pkgs.vimPlugins.nvim-spectre;
-      setupModule = "spectre";
-      setupOpts = {
-        option_name = true;
-      };
-      after = ''
-        printf("spectre loaded")
-      '';
+  config = {
+    vim.startPlugins = ["plenary-nvim"];
+    vim.lazy.plugins = {
+      "nvim-spectre" = {
+        package = pkgs.vimPlugins.nvim-spectre;
+        setupModule = "spectre";
+        setupOpts = {
+          option_name = true;
+        };
+        after = ''
+          printf("spectre loaded")
+        '';
 
-      lazy = true;
-      keys = [
-        {
-          key = "<leader>rs";
-          action = ":Telescope spectre<CR>";
-        }
-      ];
+        lazy = true;
+        keys = [
+          {
+            key = "<leader>sr";
+            action = "<cmd>lua require('spectre').toggle()<CR>";
+            mode = "n";
+          }
+        ];
+      };
     };
   };
 }
