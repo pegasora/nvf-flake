f3b9f474c4f9a162562eac158984cf988be7ac4f
dawsonb <pegasora@proton.me>
2025-10-27 20:08:22 -0700

    fixed flash.nvim
    figured out the problem with the flash.nvim plugin, needed to add extra
    config

--- config/plugins/utils/flash.nix
+++ config/plugins/utils/flash.nix
@@ -1,3 +1,28 @@
 {
-  vim.utility.motion.flash-nvim.enable = true;
+  vim.utility.motion.flash-nvim = {
+    enable = true;
+    setupOpts = {
+      modes = {
+        search = {
+          enabled = true;
+        };
+      };
+    };
+  };
+
+  vim.luaConfigRC = {
+    flash-setup = {
+      after = ["pluginConfigs"]; # This ensures it runs after other plugin setups
+      before = [];
+      data = ''
+        require("flash").setup({
+          modes = {
+            search = {
+              enabled = true
+            }
+          }
+        })
+      '';
+    };
+  };
 }
