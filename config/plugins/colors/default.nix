d16d29d4098a468f99c02825ef3dbe5801ca3fa6
pegasora <dawsonhburgess@gmail.com>
2026-02-06 12:57:58 -0800

    changed default color theme to kanagawa removed old color themes ran nix flake update

--- config/plugins/colors/default.nix
+++ config/plugins/colors/default.nix
@@ -1,8 +1,8 @@
 {
   # this will select the colorscheme
   imports = [
-    #./kanagawa.nix
-    ./catppuccin.nix
+    ./kanagawa.nix
+    #./catppuccin.nix
     #./gruvbox.nix
     #./tokyonight.nix
     #./rose-pine.nix
