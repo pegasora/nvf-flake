5da025fe41a00b829a0d3d9e4e83235953836172
pegasora <dawsonhburgess@gmail.com>
2026-02-12 14:35:13 -0800

    fixed obsidian path

--- config/plugins/git/jjsigns.nix
+++ config/plugins/git/jjsigns.nix
@@ -2,8 +2,7 @@
   pkgs,
   lib,
   ...
-}:
-let
+}: let
   jjsigns-nvim = pkgs.vimUtils.buildVimPlugin {
     name = "jjsigns.nvim";
     src = pkgs.fetchFromGitHub {
@@ -13,18 +12,17 @@ let
       sha256 = "sha256-nZu61pIkd85nISneMBy82ZZPB7Wj85Uy2LsOoWo99CE=";
     };
   };
-in
-{
-  vim.startPlugins = [ jjsigns-nvim ];
+in {
+  vim.startPlugins = [jjsigns-nvim];
 
   vim.luaConfigRC.jjsigns = lib.nvim.dag.entryAnywhere ''
     require('jjsigns').setup({
       enabled = true,
-      
+
       attach = {
         auto = true,  -- Auto-attach to JJ repository files
       },
-      
+
       signs = {
         add = { text = '┃', numhl = 'JjSignsAddNr', linehl = 'JjSignsAddLn' },
         change = { text = '┃', numhl = 'JjSignsChangeNr', linehl = 'JjSignsChangeLn' },
@@ -32,15 +30,15 @@ in
         topdelete = { text = '▔', numhl = 'JjSignsDeleteNr', linehl = 'JjSignsDeleteLn' },
         changedelete = { text = '~', numhl = 'JjSignsChangeNr', linehl = 'JjSignsChangeLn' },
       },
-      
+
       sign_priority = 6,
       signcolumn = true,
       numhl = false,
       linehl = false,
-      
+
       -- JJ specific options
       base = '@-',  -- Base revision to compare against (default: parent revision)
-      
+
       -- Performance options
       update_debounce = 100,  -- Debounce time for updates in milliseconds
     })
